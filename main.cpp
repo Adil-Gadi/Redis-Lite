94a0c62f8267a6a8359cdbfff2d9fbe1acb7cc35
Adil-Gadi <58380369+Adil-Gadi@users.noreply.github.com>
2025-02-25 20:33:24 -0800

    Adds console interface to interact with cache

--- main.cpp
+++ main.cpp
@@ -161,20 +161,39 @@ bool flush() {
     return true;
 }
 
-void extract_params(const std::string& input) {
+void extract_params(const std::string& input, int result[3]) {
 
-    int keyStart;
-    int keyEnd;
-
-    int valStart;
-    int valEnd;
-
+    int keyStart = -1;
+    int keyEnd = -1;
+    int spacesIn = 0;
+    int spacesInBefore = 0;
     for (int i = 0; i < input.length(); i++) {
-        LOG(input.at(i));
-        if (input.at(i) == '"') {
-            std::println("{} {}", i, input.at(i));
+        if (input[i] == ' ') {
+            spacesInBefore = spacesIn;
+            spacesIn++;
+        } else continue;
+
+        if (!(i + 1 < input.length())) {
+            continue;
+        }
+
+        if (spacesIn == 1 && spacesInBefore == 0 && keyStart == -1) {
+            keyStart = i + 1;
+        } else if (spacesIn == 2 && spacesInBefore == 1 && keyEnd == -1) {
+            keyEnd = i - 1;
         }
     }
+
+    if (keyEnd == -1 && keyStart != -1)
+        keyEnd = input.length() - 1;
+
+    int valueStart = -1;
+    if (keyEnd != -1 && (keyEnd + 2) < input.length())
+        valueStart = keyEnd + 2;
+
+    result[0] = keyStart;
+    result[1] = keyEnd;
+    result[2] = valueStart;
 }
 
 int main() {
@@ -182,27 +201,57 @@ int main() {
 
     while (true) {
         std::string input;
+        std::cout << "> ";
         std::getline(std::cin, input);
-
-       // LOG(input);
-
-        std::stringstream ss(input);
-
-        extract_params(input);
-        if (input.starts_with("ADD")) {
-        }
-
-        if (input.starts_with("GET")) {
-
-        }
-
-        if (input.starts_with("DELETE") || input.starts_with("REMOVE")) {
-
-        }
 
         if (input.starts_with("EXIT")) {
             break;
         }
+
+        int result[3];
+        extract_params(input, result);
+        if (result[0] == -1 || result[1] == -1) {
+            LOG("No key provided");
+            continue;
+        }
+        std::string key = input.substr(result[0], result[1] - result[0] + 1);
+        std::string value = result[2] == -1 ? "" : input.substr(result[2]);
+        if (input.starts_with("ADD")) {
+            const bool success = add_item(key.c_str(), value.c_str());
+
+            if (!success) {
+                LOG("Error: unsuccessful at adding item");
+                continue;
+            }
+
+            if (result[2] == -1) {
+                LOG("Error: no associated value provided");
+            }
+
+            continue;
+        }
+
+        if (input.starts_with("GET")) {
+            const char* value = get_item(key.c_str());
+
+            if (value == nullptr) {
+                LOG("Error: unable to find value with specified key");
+                continue;
+            }
+
+            LOG(value);
+            continue;
+        }
+
+        if (input.starts_with("DELETE") || input.starts_with("REMOVE")) {
+            bool success = delete_item(key.c_str());
+
+            if (!success) {
+                LOG("Error: unable to delete item");
+                continue;
+            }
+        }
+
     }
 
     flush();
